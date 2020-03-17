5bdf4f28b73511a5de4ec67df4d5bd65ba926e22
Ayan Sen <toayansen@gmail.com>
2020-03-17 17:13:50 +0530

    2nd commit : add DB connection with express

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
 app.get('/',(res,req)=> res.setEncoding("API running"));
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
